88c6cfff549949222c9d256148fab7bc5bbede34
weezypolak <54687555+weezypolak@users.noreply.github.com>
2024-11-27 10:12:24 -0500

    Update vite.config.ts

--- project 3/vite.config.ts
+++ project 3/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pw-test/', // Replace <repo-name> with your repository name
+  base: 'weezypolak.github.io/pw-test/', // Replace <repo-name> with your repository name
 });
